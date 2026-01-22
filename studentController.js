67fc82eef5897911136a426d74bceccf70741990
Pratiksha Wanjari <127431343+pwanjari23@users.noreply.github.com>
2026-01-22 14:44:48 +0530

    INSERT, UPDATE, DELETE task performed using sequelize

--- studentController.js
+++ studentController.js
@@ -1,4 +1,5 @@
 const db = require("../utils/db-connection");
+const Students = require("../models/students");
 
 exports.getStudents = (req, res) => {
   db.query("SELECT * FROM students", (err, results) => {
@@ -7,24 +8,45 @@ exports.getStudents = (req, res) => {
   });
 };
 
-exports.insertStudentsData = (req, res) => {
-  const { name, email, age } = req.body;
+// exports.insertStudentsData = (req, res) => {
+//   const { name, email, age } = req.body;
 
-  if (!name || !email || !age) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
+//   if (!name || !email || !age) {
+//     return res.status(400).json({ message: "All fields are required" });
+//   }
 
-  const sql = "INSERT INTO students (name, email, age) VALUES (?, ?, ?)";
+//   const sql = "INSERT INTO students (name, email, age) VALUES (?, ?, ?)";
 
-  db.query(sql, [name, email, age], (err, results) => {
-    if (err) {
-      return res.status(500).json(err);
+//   db.query(sql, [name, email, age], (err, results) => {
+//     if (err) {
+//       return res.status(500).json(err);
+//     }
+//     res.status(201).json({
+//       message: "Student inserted successfully",
+//       studentId: results.insertId,
+//     });
+//   });
+// };
+
+exports.insertStudentsData = async (req, res) => {
+  try {
+    const { name, email, age } = req.body;
+    if (!name || !email || !age) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-    res.status(201).json({
-      message: "Student inserted successfully",
-      studentId: results.insertId,
+    const students = await Students.create({
+      name: name,
+      email: email,
+      age: age,
     });
-  });
+    res
+      .status(201)
+      .send(
+        `User with name ${name} having email ${email} of ${age} years is created`,
+      );
+  } catch (error) {
+    res.status(500).send("User not created");
+  }
 };
 
 exports.getStudentById = (req, res) => {
@@ -45,45 +67,82 @@ exports.getStudentById = (req, res) => {
   });
 };
 
-exports.updateStudentById = (req, res) => {
-  const { id } = req.params;
-  const { name, email, age } = req.body;
+// exports.updateStudentById = (req, res) => {
+//   const { id } = req.params;
+//   const { name, email, age } = req.body;
 
-  if (!name || !email || !age) {
-    return res.status(400).json({ message: 'All fields are required' });
+//   if (!name || !email || !age) {
+//     return res.status(400).json({ message: "All fields are required" });
+//   }
+
+//   const sql = "UPDATE students SET name = ?, email = ?, age = ? WHERE id = ?";
+
+//   db.query(sql, [name, email, age, id], (err, results) => {
+//     if (err) {
+//       return res.status(500).json(err);
+//     }
+
+//     if (results.affectedRows === 0) {
+//       return res.status(404).json({ message: "Student not found" });
+//     }
+
+//     res.json({ message: "Student updated successfully" });
+//   });
+// };
+
+exports.updateStudentById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, age } = req.body;
+    if (!name || !email || !age) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const students = await Students.findByPk(id);
+    if (!students) {
+      return res.status(404).send("User not found");
+    }
+    students.name = name;
+    students.email = email;
+    students.age = age;
+    await students.save();
+    res.status(200).send("User updated successfully");
+  } catch (error) {
+    res.status(500).send("User cannot be updated");
   }
-
-  const sql =
-    'UPDATE students SET name = ?, email = ?, age = ? WHERE id = ?';
-
-  db.query(sql, [name, email, age, id], (err, results) => {
-    if (err) {
-      return res.status(500).json(err);
-    }
-
-    if (results.affectedRows === 0) {
-      return res.status(404).json({ message: 'Student not found' });
-    }
-
-    res.json({ message: 'Student updated successfully' });
-  });
 };
 
-exports.deleteStudentById = (req, res) => {
-  const { id } = req.params;
+// exports.deleteStudentById = (req, res) => {
+//   const { id } = req.params;
 
-  const sql = 'DELETE FROM students WHERE id = ?';
+//   const sql = "DELETE FROM students WHERE id = ?";
 
-  db.query(sql, [id], (err, results) => {
-    if (err) {
-      return res.status(500).json(err);
+//   db.query(sql, [id], (err, results) => {
+//     if (err) {
+//       return res.status(500).json(err);
+//     }
+
+//     if (results.affectedRows === 0) {
+//       return res.status(404).json({ message: "Student not found" });
+//     }
+
+//     res.json({ message: "Student deleted successfully" });
+//   });
+// };
+
+exports.deleteStudentById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const students = await Students.destroy({
+      where: {
+        id: id,
+      },
+    });
+    if(!students){
+        res.status(404).send("User not found");
     }
-
-    if (results.affectedRows === 0) {
-      return res.status(404).json({ message: 'Student not found' });
-    }
-
-    res.json({ message: 'Student deleted successfully' });
-  });
+    res.status(200).send("User deleted");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error encountered");
+  }
 };
-
